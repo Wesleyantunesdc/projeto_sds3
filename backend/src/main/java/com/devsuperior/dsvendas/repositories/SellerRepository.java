d98d863c073a685388c9aec70e13a8e393358893
Wesleyantunesdc <wesleyantunesdca@gmail.com>
2021-05-13 20:36:34 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dsvendas.repositories;
+
+import com.devsuperior.dsvendas.entities.*;
+import org.springframework.data.jpa.repository.*;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
