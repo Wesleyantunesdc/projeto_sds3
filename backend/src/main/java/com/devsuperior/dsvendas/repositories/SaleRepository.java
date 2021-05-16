05148b375eb62f006e3ed81591d1ec36cedd374a
Wesleyantunesdc <wesleyantunesdca@gmail.com>
2021-05-16 19:25:21 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dsvendas.repositories;
+
+import com.devsuperior.dsvendas.entities.*;
+import org.springframework.data.jpa.repository.*;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
