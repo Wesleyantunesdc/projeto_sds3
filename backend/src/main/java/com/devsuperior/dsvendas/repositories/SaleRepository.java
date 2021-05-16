0e4ea9339f0cba088056d3a95b85889915f39eed
Wesleyantunesdc <wesleyantunesdca@gmail.com>
2021-05-16 19:55:49 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -1,8 +1,19 @@
 package com.devsuperior.dsvendas.repositories;
 
+import com.devsuperior.dsvendas.dto.SaleSucessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.*;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.*;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
-
+	@Query("SELECT new com.devsuperior.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupBySeller();
+	
+	@Query("SELECT new com.devsuperior.dsvendas.dto.SaleSucessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSucessDTO> sucessGroupedBySeller();
 }
--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -1,8 +1,9 @@
 package com.devsuperior.dsvendas.repositories;
 
 import com.devsuperior.dsvendas.entities.*;
+
 import org.springframework.data.jpa.repository.*;
 
 public interface SellerRepository extends JpaRepository<Seller, Long>{
-
+	
 }
